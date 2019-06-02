bda58f78204c3c7c112799d9b39a61b34b350dfd
Srikanth Kamath <Srikanth@tskamath.com>
2019-06-02 11:28:14 +0530

    edit the path for template-parts

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -51,9 +51,9 @@ function tsktwo_infinite_scroll_render() {
 	while ( have_posts() ) {
 		the_post();
 		if ( is_search() ) :
-			get_template_part( 'template-parts/content', 'search' );
+			get_template_part( 'template-parts/content/content', 'search' );
 		else :
-			get_template_part( 'template-parts/content', get_post_type() );
+			get_template_part( 'template-parts/content/content', get_post_type() );
 		endif;
 	}
 }
